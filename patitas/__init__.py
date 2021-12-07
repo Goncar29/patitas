0cc9bfec6496adfa67aa97f279d2f977a7f8a43e
Carlos Gonzalez <51546329+Goncar29@users.noreply.github.com>
2021-12-06 21:54:02 -0300

    Delete patitas directory

--- patitas/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.secret_key = 'estoesunapassword1'
-app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./data/basededatos.db'
-
-from patitas import models
-from patitas import views
